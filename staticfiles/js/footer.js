6be28d4bf7bcb1cc58b80815f422d607e4e2f603
mahsakhodadadi17 <mahsakhodadadi17@gmail.com>
2026-07-22 16:53:08 +0330

    add gitignore and remove cache files

--- staticfiles/js/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    const currentYear = document.getElementById("currentYear");
-
-    if (currentYear) {
-        currentYear.textContent = new Date().getFullYear();
-    }
-
-});
